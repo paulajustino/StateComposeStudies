a937523be392afab4e976df65ce7e7936ea7e66a
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2024-01-12 14:08:17 -0300

    studying more about state

--- app/src/main/java/com/example/statecomposestudies/WaterCounter.kt
+++ app/src/main/java/com/example/statecomposestudies/WaterCounter.kt
@@ -10,20 +10,34 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.statecomposestudies.ui.theme.StateComposeStudiesTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
 
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text(text = "You've had $count water glasses.")
+
+        if (count > 0) {
+            Text(text = "You've had $count water glasses.")
+        }
+
         Button(
             modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10,
             onClick = { count++ }
         ) {
             Text(text = "Add one")
         }
     }
+}
 
+@Preview
+@Composable
+fun WaterCounterPreview() {
+    StateComposeStudiesTheme {
+        WaterCounter()
+    }
 }
