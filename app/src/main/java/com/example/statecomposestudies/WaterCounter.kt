0fa3f921f0241c566e0a40f22f847c672bad70cb
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2024-01-12 15:55:55 -0300

    implement state hoisting

--- app/src/main/java/com/example/statecomposestudies/WaterCounter.kt
+++ app/src/main/java/com/example/statecomposestudies/WaterCounter.kt
@@ -10,33 +10,35 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.statecomposestudies.ui.theme.StateComposeStudiesTheme
+
+// função responsável por armazenar e modificar o estado
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(
+        count = count,
+        onIncrement = { count++ },
+        modifier = modifier
+    )
+}
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier)
+{
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-
         if (count > 0) {
             Text(text = "You've had $count water glasses.")
         }
-
         Button(
-            enabled = count < 10,
-            onClick = { count++ }
+            onClick = onIncrement,
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text(text = "Add one")
         }
-    }
-}
-
-@Preview
-@Composable
-fun WaterCounterPreview() {
-    StateComposeStudiesTheme {
-        WaterCounter()
     }
 }
