1523007d089b3a14d95729a2ef71f9a1ca633aa6
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2024-01-15 10:24:47 -0300

    implement list and lazycolumn

--- app/src/main/java/com/example/statecomposestudies/WellnessTaskItem.kt
+++ app/src/main/java/com/example/statecomposestudies/WellnessTaskItem.kt
@@ -4,17 +4,40 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
+fun StatefulWellnessTaskItem(
+    taskName: String,
+    modifier: Modifier = Modifier
+) {
+   var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = { /*TODO*/ },
+        modifier = modifier
+    )
+}
+
+@Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -27,6 +50,10 @@ fun WellnessTaskItem(
             modifier = Modifier
                 .weight(1f)
                 .padding(start = 16.dp)
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
             Icon(
